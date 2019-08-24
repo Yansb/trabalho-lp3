8751e9a4dd1b19bfb04d3f2d4c70a55922f8ef31
Matheus santos <msdsantos96@gmail.com>
2019-08-24 11:11:50 -0300

    Correção

--- Controller/ControladorIndex.php
+++ Controller/ControladorIndex.php
@@ -1,25 +1,50 @@
 <?php
-include "../Model/class.php"; 
+        include "../Model/class.php"; 
 
-$Acao= $_POST['acao'] ; 
+        $Acao= $_POST['Acao'] ; 
 
-if($Acao==="NovoChamado"){
-    $Ataul = new DateTime(); 
-    $DataHoraAbertura= $Ataul->format('d-m-Y H:i:s'); 
-    $Nome=$_POST["Nome"];
-    $CPF=$_POST["CPF"];
-    $Email= $_POST["Email"];
-    $Telefone= $_POST["Telefone"];
-    $Setor= $_POST["Setor"];
-    $Problema=$_POST["Problema"];
-    $Arquivo= isset( $_POST["Arquivo"]); 
-    $Descricao=$_POST['Descricao'];
-    $OBS=$_POST["OBS"]; 
-  
-    $Chamado= new Chamado($Descricao,$Setor,$Problema,$Arquivo,$OBS,$DataHoraAbertura,"" ); 
-    $User= new Usuario($Nome,$CPF,$Telefone,$Email);
+        if($Acao==="NovoChamado"){
+            $Ataul = new DateTime(); 
+            $DataHoraAbertura= $Ataul->format('d-m-Y H:i:s'); 
+            $Nome=$_POST["Nome"];
+            $CPF=$_POST["CPF"];
+            $Email= $_POST["Email"];
+            $Telefone= $_POST["Telefone"];
+            $Setor= $_POST["Setor"];
+            $Problema=$_POST["Problema"];
+            $Arquivo= isset( $_POST["Arquivo"]); 
+            $Descricao=$_POST['Descricao'];
+            $OBS=$_POST["OBS"]; 
+        
+            $Chamado= new Chamado("" ,$Descricao,$Setor,$Problema,$Arquivo,$OBS,$DataHoraAbertura); 
+            $User= new Usuario($Nome,$CPF,$Telefone,$Email);
 
 
-}
+        }
+
+        if($Acao==="AdicionarFT"){
+
+            $Ataul = new DateTime(); 
+            $DataHoraAbertura= $Ataul->format('d-m-Y H:i:s'); 
+            $Nome=$_POST["Nome"];
+            $CPF=$_POST["CPF"];
+            $Email= $_POST["Email"];
+            $Telefone= $_POST["Telefone"];
+            $Setor= $_POST["Lab"];
+            $Descricao=$_POST['Software'];
+            $Link=$_POST['Link'];
+            $Plugin=$_POST['Plugin'];
+        
+            $ChamadoSoftware= new ChamadoSoftware("" ,$Descricao,$Setor,"dad","adad","dasda",$DataHoraAbertura,$Link,$Plugin); 
+            $User= new Usuario($Nome,$CPF,$Telefone,$Email);
+
+            echo "Desc: ".$ChamadoSoftware->getOBS()."<br>";
+          
+
+        
+
+        }
+
+
 
 ?>
