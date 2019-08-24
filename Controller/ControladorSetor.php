3ea2fc452930541f6378272363062529440cb9d8
Matheus Souza dos Santos <msdsantos96@gmail.com>
2019-08-24 02:45:35 -0300

    Add ControladorSetor

---
+++ Controller/ControladorSetor.php
@@ -0,0 +1,18 @@
+<?php 
+include '../Model/class.php';
+
+$Acao=$_POST['Acao'];
+
+if($Acao==="Adicionar"){
+    $Nome=$_POST['Nome'];
+    $Telefone=$_POST['Telefone'];
+    $Email=$_POST['Email'];
+
+
+    $Setor= new Setor("",$Nome, $Email,$Telefone); 
+    echo $Setor->getEmail(); 
+}
+
+
+
+?>
