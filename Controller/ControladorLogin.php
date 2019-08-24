5e33b7fa4be43ddf0fc6b5c7e4217b8d17b14100
Matheus Souza dos Santos <msdsantos96@gmail.com>
2019-08-24 01:46:01 -0300

    ADD ControladorLogin

---
+++ Controller/ControladorLogin.php
@@ -0,0 +1,19 @@
+<?php
+require "../Model/class.php"; 
+$Acao = $_POST['Acao']; 
+
+if($Acao==="Logar"){
+
+    $Login = $_POST['Login'];
+    $Senha = $_POST['Senha'];
+  
+    $Tecnico= new Tecnico("","","","",$Login,$Senha); 
+   
+}
+else{
+
+}
+
+
+
+?>
