2d7600c23ae13737375a6b7564243d6784a74210
Matheus Souza dos Santos <msdsantos96@gmail.com>
2019-08-24 02:30:57 -0300

    add ControladorTecnico

---
+++ Controller/ControladorTecnico.php
@@ -0,0 +1,24 @@
+
+<?php
+
+require '../Model/class.php';
+
+$Acao= $_POST['Acao']; 
+
+if($Acao==="Adicionar"){
+    $CPF=$_POST["CPF"];
+    $Cargo = $_POST['Cargo'];
+    $Nome = $_POST['Nome'];
+    $Login = $_POST['Login'];
+    $Senha = $_POST['Senha'];
+    $Email = $_POST['Email'];
+    $Telefone= $_POST["Telefone"];
+    $Setor= $_POST["Setor"]; 
+
+    $Tecnico = new Tecnico($Nome,$CPF,$Telefone,$Email,$Login,$Senha,$Cargo,$Setor); 
+
+    header('location: ../View/CadastroTecnico.php');
+
+}
+
+?>
