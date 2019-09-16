7be9ec78a1a77c786ffd8aebd988263bc89146b4
BrunoPittaF <caiobrunopittaf@gmail.com>
2019-09-16 17:29:21 -0300

    Código de ordenação de tabela

--- View/Chamados.php
+++ View/Chamados.php
@@ -10,6 +10,8 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/MYB.css">
+    <!-- NÃO retirem essa linha <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css"> -->
+
     <?php
     require_once "../Model/ClasseChamados.php";
     require_once "../Model/ClassUsuarios.php";
@@ -162,7 +164,7 @@
     <br><br><br><br><br>
     <div class="chamados">
         <section>
-            <table class="table">
+            <table class="table" id="tabela" name="tabela">
                 <thead class="thead-dark">
                     <tr>
                         <th scope="col">Numero chamado</th>
@@ -225,7 +227,9 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="js/validacao.js"></script>
-
+    <script src="js/tabela.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+    
 
 
 
---
+++ View/js/tabela.js
@@ -0,0 +1,13 @@
+$(document).ready(function cssOrdenacao() {
+    $('#tabela').DataTable( {
+        paging:   false,
+        ordering: true,
+        info:     false,
+        searching: false
+    } );
+} );
+
+$(document).ready( function ordenacao () {
+    $('#tabela').DataTable();
+} );
+
