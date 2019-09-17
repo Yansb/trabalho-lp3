08eb598e0ce7fc1be9a5ab88b7f551b90081531c
Caio Bruno Pitta Figueiredo <44136082+BrunoPittaF@users.noreply.github.com>
2019-09-17 07:55:57 -0300

    Usuario ordenando tabela

--- View/usuario.php
+++ View/usuario.php
@@ -107,7 +107,7 @@
     <div class="chamados">
       <h1 style="color:rgb(41, 33, 24)">Chamados: <?php echo $_SESSION["Usuario"]->getNome(); ?> </h1>
       <section>
-        <table class="table">
+        <table class="table"  id="tabela" name="tabela">
           <thead class="thead-dark">
             <tr>
               <th scope="col">Numero chamado</th>
@@ -151,6 +151,9 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   <script src="js/validacao.js"></script>
+  <script src="js/tabela.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+    
 
 </footer>
 
